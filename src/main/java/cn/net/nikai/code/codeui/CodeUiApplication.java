2c28c751e70169eb39cc4b59833b98ef01e2e4ad
nikai <keni1006@163.com>
2018-11-09 00:07:53 +0800

    init project

---
+++ src/main/java/cn/net/nikai/code/codeui/CodeUiApplication.java
@@ -0,0 +1,12 @@
+package cn.net.nikai.code.codeui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeUiApplication.class, args);
+	}
+}
